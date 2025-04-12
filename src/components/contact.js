7203ae0dd2a9d0e64b8cce1760ba006575b6ba40
YASSIRELMAHDAOUI <yassirelmahdaoui1@gmail.com>
2025-04-12 10:36:12 +0100

    ajout du fichier contat us avec finalisation du projet front end voiture

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
-function Contact (){
-    return(
-        <>
-        <h1>BONJOUR DANS LA PAGE CONTAT</h1>
-        <Link to={'/home'}>retour a la page d'acceuil</Link>
-        </>
-    )
-}
-export default Contact
+import React, { useState } from 'react';
+
+const ContactUs = () => {
+  const [nom, setNom] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [sms, setSms] = useState('');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+      setSms(`${nom} ${email} ${message} : est envoyer avec succes!!!!`)
+      setTimeout(() => {
+        setSms('');
+      }, 3600);
+      }
+  return (
+    
+    <div className="container mt-5">
+      <h2 className="text-center mb-4">Contact Us</h2>
+      <form className="shadow p-4 rounded bg-light">
+        <div className="mb-3">
+        <p className='alert alert-success text-center" role="alert' >{sms}</p>
+          <label htmlFor="name" className="form-label">Nom</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            name="name"
+            value={nom}
+            onChange={(e) => setNom(e.target.value)}
+            placeholder="Entrez votre nom"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Adresse Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Entrez votre email"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="message" className="form-label">Message</label>
+          <textarea
+            className="form-control"
+            id="message"
+            name="message"
+            rows="5"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Écrivez votre message ici..."
+            required
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary w-100" onClick={handleClick}>Envoyer</button>
+        
+      </form>
+    </div>
+  );
+};
+
+export default ContactUs;
